5b4327a309b05dfbb4316e718c5d804a4831c7b2
garvit1166 <92694655+garvit1166@users.noreply.github.com>
2024-03-29 20:25:50 +0530

    Update signup.js

--- src/pages/Signup/signup.js
+++ src/pages/Signup/signup.js
@@ -7,6 +7,7 @@ import { useState } from 'react';
 import useValue from '../../context/userContext';
 import { useNavigate } from 'react-router-dom';
 import useUser from '../../context/userContext';
+import Cookies from "js-cookie";
 
 function Signup() {
   let navigate = useNavigate();
